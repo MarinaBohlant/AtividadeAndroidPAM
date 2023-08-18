f250a8d4ad17ed538187a4f9cb747c64e5d78ba5
3JD <3jd@bglab.local>
2023-08-18 12:11:32 -0300

    Alterações

--- app/src/main/java/com/example/appexerc/MainActivity.java
+++ app/src/main/java/com/example/appexerc/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.appexerc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtInicio;
+    private Button btnExerc1, btnExerc2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnExerc1 = findViewById(R.id.btnExerc1);
+
+        btnExerc1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent Exerc1 = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(Exerc1);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/appexerc/MainActivity2.java
+++ app/src/main/java/com/example/appexerc/MainActivity2.java
@@ -3,12 +3,35 @@ package com.example.appexerc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private TextView txtView1, txtView2, txtResult;
+    private EditText txtNum1, txtNum2;
+    private Button btnVerificar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        txtNum1 = findViewById(R.id.txtNum1);
+        txtNum2 = findViewById(R.id.txtNum2);
+    }
+
+    public void onClick(View v){
+        int vl1 = Integer.parseInt(txtNum1.toString());
+        int vl2 = Integer.parseInt(txtNum2.toString());
+
+        if (vl1 % vl2 == 0 || vl2 % vl1 == 0) {
+            txtResult.setText("Esses números são múltiplos.");
+        }
+        else{
+            txtResult.setText("Esses números não são múltiplos.");
+        }
     }
 }
